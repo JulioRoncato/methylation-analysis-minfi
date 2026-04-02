93f99fc21a6d56bc78495a3132b90cbf735cef6d
Julio Roncato <julioroncato08@gmail.com>
2026-04-02 10:33:10 -0300

    Refactor data import QC script for clarity

--- scripts/01_data_import_qc.R
+++ scripts/01_data_import_qc.R
@@ -1,7 +1,33 @@
-'Load necessary libraries\nlibrary(minfi)\nlibrary(ggplot2)\n\n
-# Define function to load IDAT files\nload_idat_files <- function(idat_dirs) {\n  rg_set <- read.metharray.exp(idat_dirs)\n  return(rg_set)\n}\n\n
-# Perform quality control\nperform_quality_control <- function(rg_set) {\n  
-# Basic QC metrics\n  qc_metrics <- getQC(rg_set)\n  \n  
-# Print summary of QC metrics\n  print(qc_metrics)\n\n  
-# Generate QC plots\n  png(filename = "qc_report.png")\n  plotQC(rg_set)\n  dev.off()\n  \n  return(qc_metrics)\n}\n\n
-# Example usage\nidat_dirs <- "path/to/idat/files"\nrg_set <- load_idat_files(idat_dirs)\nqc_results <- perform_quality_control(rg_set)\n'
+Load necessary libraries
+library(minfi)
+library(ggplot2)
+
+# Define function to load IDAT files
+load_idat_files <- function(idat_dirs) {
+rg_set <- read.metharray.exp(idat_dirs)
+return(rg_set)
+}
+
+# Perform quality control
+perform_quality_control <- function(rg_set) {
+  
+# Basic QC metrics
+  qc_metrics <- getQC(rg_set)
+
+  
+# Print summary of QC metrics
+  print(qc_metrics)
+  
+# Generate QC plots
+  png(filename = "qc_report.png")
+  plotQC(rg_set)
+  dev.off()
+  
+  return(qc_metrics)
+}
+
+# Example usage
+idat_dirs <- "path/to/idat/files"
+rg_set <- load_idat_files(idat_dirs)
+qc_results <- perform_quality_control(rg_set)
+'
